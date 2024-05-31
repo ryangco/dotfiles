fa5a55f3d01641b596ca93b979443c5020deacee
ryangco <135596330+ryangco@users.noreply.github>
2024-05-31 18:40:41 +0800

    feat(dap): added keymaps to widgets, removed trouble.nvim

--- neovim/.config/nvim/lua/rogie/plugins/assistants.lua
+++ neovim/.config/nvim/lua/rogie/plugins/assistants.lua
@@ -23,7 +23,6 @@ return {
 		dependencies = {
 			"MunifTanjim/nui.nvim",
 			"nvim-lua/plenary.nvim",
-			"folke/trouble.nvim",
 			"nvim-telescope/telescope.nvim",
 		},
 	},
--- neovim/.config/nvim/lua/rogie/plugins/debug.lua
+++ neovim/.config/nvim/lua/rogie/plugins/debug.lua
@@ -1,7 +1,5 @@
 return {
-	-- NOTE: Yes, you can install new plugins here!
 	"mfussenegger/nvim-dap",
-	-- NOTE: And you can specify dependencies as well
 	dependencies = {
 		"rcarriga/nvim-dap-ui",
 		"theHamsta/nvim-dap-virtual-text",
@@ -24,10 +22,38 @@ return {
 		vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
 		vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
 		vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
-		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-		vim.keymap.set("n", "<leader>B", function()
+		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+		vim.keymap.set("n", "<leader>dB", function()
 			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
 		end, { desc = "Debug: Set Breakpoint" })
+		vim.keymap.set("n", "<leader>dus", function()
+			local widgets = require("dap.ui.widgets")
+			local sidebar = widgets.sidebar(widgets.scopes)
+			sidebar.open()
+		end, { desc = "Open debug sidebar" })
+		vim.keymap.set("n", "<leader>dlp", function()
+			require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+		end, { desc = "Debug Log Point" })
+		vim.keymap.set("n", "<leader>ddr", function()
+			require("dap").repl.open()
+		end, { desc = "Debug REPL Open" })
+		vim.keymap.set("n", "<leader>ddl", function()
+			require("dap").run_last()
+		end, { desc = "Debug Debug Last" })
+		vim.keymap.set({ "n", "v" }, "<leader>ddh", function()
+			require("dap.ui.widgets").hover()
+		end, { desc = "Hover Widget" })
+		vim.keymap.set({ "n", "v" }, "<leader>ddp", function()
+			require("dap.ui.widgets").preview()
+		end, { desc = "Preview Widget" })
+		vim.keymap.set("n", "<leader>ddf", function()
+			local widgets = require("dap.ui.widgets")
+			widgets.centered_float(widgets.frames)
+		end, { desc = "Frames Float" })
+		vim.keymap.set("n", "<leader>dds", function()
+			local widgets = require("dap.ui.widgets")
+			widgets.centered_float(widgets.scopes)
+		end, { desc = "Scopes Widget" })
 
 		dapui.setup({
 			icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
