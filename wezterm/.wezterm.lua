759c2c983de4cc15b20b225b26ad20dfb0e92346
ryangco11 <135596330+ryangco@users.noreply.github.com>
2025-02-19 21:05:11 +0800

    wez

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,21 +1,13 @@
--- Pull in the wezterm API
-
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
-
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-
--- config.color_scheme = "AdventureTime"
-
+config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("Iosevka Nerd Font Mono", { weight = "Medium" })
 config.font_size = 10.5
 config.hide_tab_bar_if_only_one_tab = true
-
--- and finally, return the configuration to wezterm
+-- config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.default_cursor_style = "BlinkingBar"
 
 return config
